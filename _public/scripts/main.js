2d3acb533344887b0e9b0cd4a12fde9fc6c315f4
Rayner Tan <raynertanxw@me.com>
2025-01-01 12:12:54 +0800

    Updated with project years

--- _public/scripts/main.js
+++ _public/scripts/main.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
   $('.project').hover(
     function () {
       $(this).find('.description').animate({
-        top:"210px"
+        top:"180px"
       }, {duration: 200, queue: false});
     },
     function () {
